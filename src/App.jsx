74a811a52af113a08ad026c06c0671ef1e918f5a
Melanie-J-Baker <baker_mel@hotmail.com>
2023-10-16 18:19:35 +0100

    Create GameMenu and Card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-import '../styles/App.css'
-import Header from './Header'
+import './styles/App.css'
+import Header from './components/Header'
+import GameArea from './components/GameArea'
 
 function App() {
   return (
     <div className='main'>
       <Header />
+      <GameArea />
     </div>
   )
 }
---
+++ src/components/GameArea.jsx
@@ -0,0 +1,10 @@
+import Card from './Card';
+
+export default function GameArea() {
+    return (
+        <div className="cards">
+            <Card />
+            <Card />
+        </div>
+    )
+}
