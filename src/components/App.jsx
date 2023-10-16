e8e2960cf5341454528c93f61e573378555607df
Melanie-J-Baker <baker_mel@hotmail.com>
2023-10-16 17:49:27 +0100

    Create header component and add fonts and background image

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import '../styles/App.css'
+import Header from './Header'
+
+function App() {
+  return (
+    <div className='main'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div className='header'>
+            <h1> FUTURAMA MEMORY GAME</h1>
+            <div className="scores">
+                <p className='score'>Score: /12</p>
+                <p className='highScore'>High Score: /12</p>
+            </div>
+        </div>
+    )
+}
