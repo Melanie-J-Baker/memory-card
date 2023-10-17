011f6fb3b7faf052c59cd236d5f8fe6237697cb9
Melanie-J-Baker <baker_mel@hotmail.com>
2023-10-17 08:00:21 +0100

    Style result button

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,10 +1,10 @@
 export default function Result({ restart, result }) {
     return (
         <div className={result === 'win' ? 'win' : 'lose'}>
-            <p>{result === 'win' ? 'You win!!' : 'You lose!'}</p>
-            <button onClick={() => {
+            <p className="result">{result === 'win' ? 'You win!!' : 'You lose!'}</p>
+            <button className="result-btn" onClick={() => {
                 restart();
-            }}>Restart Game</button>
+            }}>Restart</button>
         </div>
     )  
 }
