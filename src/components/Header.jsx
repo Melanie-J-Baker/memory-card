3d6f0b0f6940b965f5be235c74acc07fd5e79ae7
Melanie-J-Baker <baker_mel@hotmail.com>
2023-10-18 12:48:08 +0100

    Change high score text to 'best' and correct auto margin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({score, highScore}) {
             <h1>FUTURAMA MEMORY GAME</h1>
             <div className="scores">
                 <p className='score'>Score: {score}/10</p>
-                <p className='highScore'>High Score: {highScore}/10</p>
+                <p className='highScore'>Best: {highScore}/10</p>
             </div>
         </div>
     )
